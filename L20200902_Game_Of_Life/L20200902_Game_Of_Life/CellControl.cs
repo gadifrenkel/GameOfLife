d3ad8efeb1b0c862f3b4c7a1b00ca70f4f8043ff
Gadi Frenkel <47473384+gadifrenkel@users.noreply.github.com>
2020-09-07 23:50:54 +0300

    Add files via upload

---
+++ L20200902_Game_Of_Life/L20200902_Game_Of_Life/CellControl.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace L20200902_Game_Of_Life
+{
+    class CellControl : Control
+    {
+        private int row, col;
+        private CellControl[,] cells;
+        private bool isAlive;
+        private Lockable lockable;
+        private bool nextGeneration;
+        public CellControl(CellControl[,] cells, int row, int col, Lockable lockable)
+        {
+            
+            this.row = row;
+            this.col = col;
+            this.cells = cells;
+            isAlive = false;
+            UpdateColor();
+            this.Paint += CellControl_Paint;
+            this.MouseClick += CellControl_MouseClick;
+            this.lockable = lockable;
+        }
+        public void CalculateNextGeneration()
+        {
+            int aliveNeighbors = 0;
+            int ROWS = cells.GetLength(0);
+
+            //the row above me:
+            if(row > 0)
+            {
+                if (col > 0)
+                    if (cells[row - 1, col - 1].isAlive)
+                        aliveNeighbors++;
+                if (cells[row - 1, col].isAlive)
+                    aliveNeighbors++;
+                if (col < ROWS-1)//not last column
+                    if (cells[row - 1, col + 1].isAlive)
+                        aliveNeighbors++;
+            }
+
+            //my own row:
+            if (col > 0)
+                if (cells[row, col - 1].isAlive)
+                    aliveNeighbors++;
+            if (col < ROWS - 1)//not last column
+                if (cells[row, col + 1].isAlive)
+                    aliveNeighbors++;
+
+            //the row beneath me
+            if (row < ROWS - 1)
+            {
+                if (col > 0)
+                    if (cells[row + 1, col - 1].isAlive)
+                        aliveNeighbors++;
+                if (cells[row + 1, col].isAlive)
+                    aliveNeighbors++;
+                if (col < ROWS - 1)//not last column
+                    if (cells[row + 1, col + 1].isAlive)
+                        aliveNeighbors++;
+            }
+
+            nextGeneration = isAlive;
+            if (isAlive)
+            {
+                if (aliveNeighbors <= 1)
+                    nextGeneration = false;//die from loneliness
+
+                if (aliveNeighbors > 3)
+                    nextGeneration = false;//die from over population.
+            }
+            else
+            {
+                if (aliveNeighbors == 3)
+                    nextGeneration = true;
+
+            }
+        }
+
+        public void MoveToNextGeneration()
+        {
+            isAlive = nextGeneration;
+            UpdateColor();
+        }
+        private void CellControl_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (lockable.IsLocked())
+                return;
+            isAlive = !isAlive;
+            UpdateColor();
+        }
+
+        
+
+        private void UpdateColor()
+        {
+            BackColor = isAlive ? Color.Black : Color.White;
+        }
+
+        private void CellControl_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(Pens.Gray, 0, 0, Width-1, Height-1);
+        }
+    }
+}
---
+++ L20200902_Game_Of_Life/L20200902_Game_Of_Life/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace L20200902_Game_Of_Life
+{
+    public partial class Form1 : Form, Lockable
+    {
+        public const int ROWS = 10;
+        private CellControl[,] cells;
+        private Button btnStart;
+        private bool locked;
+        private bool go;
+
+        public Form1()
+        {
+            cells = new CellControl[ROWS,ROWS];
+            locked = false;
+            InitializeComponent();
+            go = true;
+        }
+
+
+
+        
+        public bool IsLocked()
+        {
+            return locked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            const int CELL_SIZE = 25;//the size of the cell in pixels
+            int leftMargin = (this.Width - CELL_SIZE * ROWS) / 2;
+            int y = 50;
+            for (int i = 0; i < ROWS; i++)
+            {
+                for (int j = 0; j < ROWS; j++)
+                {
+                    CellControl cellControl = new CellControl(cells, i, j, this);
+                    cellControl.Top = y;
+                    cellControl.Left = leftMargin + j * CELL_SIZE;
+                    cellControl.Width = CELL_SIZE;
+                    cellControl.Height = CELL_SIZE;
+                    cells[i, j] = cellControl;
+                    Controls.Add(cellControl);
+                }
+                y += CELL_SIZE;
+            }
+            btnStart = new Button();
+            btnStart.Text = "Start";
+            btnStart.Top = Height - btnStart.Height - 50;
+            btnStart.Left = (Width - btnStart.Width) / 2;
+            Controls.Add(btnStart);
+            btnStart.Click += BtnStart_Click;
+
+
+
+        }
+
+        private void NextGeneration()
+        {
+            for (int i = 0; i < ROWS; i++)
+            {
+                for (int j = 0; j < ROWS; j++)
+                {
+                    cells[i, j].CalculateNextGeneration();
+                }
+            }
+            for (int i = 0; i < ROWS; i++)
+            {
+                for (int j = 0; j < ROWS; j++)
+                {
+                    cells[i, j].MoveToNextGeneration();
+                }
+            }
+
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            locked = true;
+            btnStart.Enabled = false;
+            Thread thread = new Thread(()=> {
+                while (go)
+                {
+                    Thread.Sleep(500);
+                    Action action = new Action(() => {
+                        this.NextGeneration();
+                    });
+                    //this.Invoke(action);
+
+                    Dispatcher.CurrentDispatcher.Invoke(() =>
+
+                    {
+                        action();
+                    });
+
+                }
+            });
+            thread.Start();
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            go = false;
+        }
+    }
+
+    public interface Lockable
+    {
+        bool IsLocked();
+    }
+}
